5b582792539f7f31c327e8e6a9770757d4b898c7
atahmedtarek46-source <atahmedtarek46@gmail.com>
2026-01-09 20:51:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function askAI() {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + API_KEY,
+     "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + API_KEY,
       {
         method: "POST",
         headers: {
